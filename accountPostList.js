1a7ef0c710640dfa2520abd70888ddf976e6556d
James-Livesey <jamesliveg@gmail.com>
2018-12-22 12:05:20 +0000

    Fix images not showing up

--- accountPostList.js
+++ accountPostList.js
@@ -1,14 +1,14 @@
 var postList = [];
 
-function addPost(name, by, byStaff = false, thumbnail = "media/NoThumbnail.png", link = "javascript:alert('Sorry! This post is unavailable.');", verified = false) {
+function addPost(name, by, byStaff = false, image = "media/NoImage.png", link = "javascript:alert('Sorry! This post is unavailable.');", verified = false) {
     $("#postLoader").hide();
 
     if (verified) {
         $("#postList").append(`
             <a class="hidden">
                 <div class="item postItem">
-                    <div class="itemThumbnailHolder">
-                        <img src="media/NoThumbnail.png" class="itemThumbnail" />
+                    <div class="itemImageHolder">
+                        <img src="media/NoImage.png" class="itemImage" />
                     </div>
                     <h2 class="cutOff"><span class="itemTitle">Loading...</span> <i class="material-icons">verified_user</i></h2>
                     <span class="postByPrefix"></span><span class="postBy">Loading...</span>
@@ -19,8 +19,8 @@ function addPost(name, by, byStaff = false, thumbnail = "media/NoThumbnail.png",
         $("#postList").append(`
             <a class="hidden">
                 <div class="item postItem">
-                    <div class="itemThumbnailHolder">
-                        <img src="media/NoThumbnail.png" class="itemThumbnail" />
+                    <div class="itemImageHolder">
+                        <img src="media/NoImage.png" class="itemImage" />
                     </div>
                     <h2 class="cutOff"><span class="itemTitle">Loading...</span></h2>
                     <span class="postByPrefix"></span><span class="postBy">Loading...</span>
@@ -37,7 +37,7 @@ function addPost(name, by, byStaff = false, thumbnail = "media/NoThumbnail.png",
         $("#postList").children().last().find(".postBy").css("color", "#27ef70");
     }
 
-    $("#postList").children().last().find("img").attr("src", thumbnail);
+    $("#postList").children().last().find("img").attr("src", image);
     $("#postList").find("a").last().attr("href", link);
 }
 
@@ -62,9 +62,9 @@ function showAll() {
         } else {
             for (var i = 0; i < postList.length; i++) {
                 if (postList[i]["by"] == undefined) {
-                    addPost(postList[i]["title"], "Anonymous", postList[i]["thumbnail"], "post.html?play=" + postList[i]["key"], postList[i]["verified"]);
+                    addPost(postList[i]["title"], "Anonymous", postList[i]["image"], "post.html?play=" + postList[i]["key"], postList[i]["verified"]);
                 } else {
-                    addPost(postList[i]["title"], postList[i]["by"].substring(0, 30), postList[i]["byStaff"], postList[i]["thumbnail"], "post.html?play=" + postList[i]["key"], postList[i]["verified"]);
+                    addPost(postList[i]["title"], postList[i]["by"].substring(0, 30), postList[i]["byStaff"], postList[i]["image"], "post.html?play=" + postList[i]["key"], postList[i]["verified"]);
                 }
             }
         }
